d127586d3681fdc7e0f8b09f4d1b514f09a104d6
ana.disigant <anadisigant@gmail.com>
2025-05-29 15:02:08 -0300

    fixing buttons

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function verificar(elemento, isIA) {
   respondido = true;
 
   const respostas = document.querySelectorAll("p");
+  const botao = document.getElementById("botaoAcao");
+
   respostas.forEach(p => {
     if (p === elemento) {
       p.classList.add(isIA ? "certa" : "errada");
@@ -12,4 +14,26 @@ function verificar(elemento, isIA) {
       p.classList.add("errada");
     }
   });
+
+  botao.disabled = false;
+  botao.classList.add("ativado");
+
+  if (isIA) {
+    botao.textContent = "Próxima";
+    botao.onclick = () => window.location.href = 'question7.html';
+  } else {
+    botao.textContent = "Refazer";
+    botao.onclick = () => {
+      respostas.forEach(p => {
+        p.classList.remove("certa", "errada");
+        p.style.pointerEvents = "auto";
+      });
+
+      respondido = false;
+      botao.disabled = true;
+      botao.textContent = "Próxima";
+      botao.classList.remove("ativado");
+      botao.onclick = () => {}; 
+    };
+  }
 }
